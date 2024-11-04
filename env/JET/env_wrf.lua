cf3c9a7d670013b0eef7db6ea45700cf8ce13bf4
Shawn Murdzek <shawn.s.murdzek@noaa.gov>
2024-11-04 14:40:00 +0000

    Update JET environment to remove remaining /lfs4 dependencies and use a newer hdf5 version
    The previous WRF environment on JET is still retained in env_wrf_OLD.lua. Note that ungrib ingnores pressure levels above 100 hPa when using the new environment, which results in downstream crashes. It is recommended that files are ungribbed using an older version of ungrib

--- env/JET/env_wrf.lua
+++ env/JET/env_wrf.lua
@@ -1,31 +1,31 @@
 help([[
 This module loads libraries for building WRF on
-the JET using Intel-2022.1.2 (Rocky 8)
+the JET using Intel-2021.5.0 (Rocky 8)
 ]])
 
 whatis([===[Loads libraries needed for building WRF on Jet ]===])
 
-load(pathJoin("intel", os.getenv("intel_ver") or "2022.1.2"))
-load(pathJoin("impi", os.getenv("impi_ver") or "2022.1.2"))
+append_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.6.0/envs/unified-env-rocky8/install/modulefiles/Core")
+load(pathJoin("stack-intel", os.getenv("stack_intel_ver") or "2021.5.0"))
+load(pathJoin("stack-intel-oneapi-mpi", os.getenv("stack_intel_oneapi_mpi_ver") or "2021.5.1"))
 load(pathJoin("netcdf", os.getenv("netcdf_ver") or "4.7.0"))
-load(pathJoin("hdf5", os.getenv("hdf5_ver") or "1.10.6"))
+load(pathJoin("hdf5", os.getenv("hdf5_ver") or "1.14.0"))
 
-append_path("MODULEPATH", "/mnt/lfs4/HFIP/hfv3gfs/nwprod/NCEPLIBS/modulefiles")
-load(pathJoin("wgrib2", os.getenv("wgrib2_ver")))
-load(pathJoin("g2", os.getenv("g2_ver") or "v3.1.0"))
-load(pathJoin("g2tmpl", os.getenv("g2tmpl_ver") or "v1.6.0"))
-load(pathJoin("jasper", os.getenv("jasper_ver") or "v1.900.1"))
-load(pathJoin("png", os.getenv("png_ver") or "v1.2.44"))
-load(pathJoin("z", os.getenv("z_ver") or "v1.2.6"))
-load(pathJoin("w3emc", os.getenv("w3emc_ver") or "v2.4.0"))
-load(pathJoin("w3nco", os.getenv("w3nco_ver") or "v2.0.6"))
-load(pathJoin("bufr", os.getenv("bufr_ver") or "v11.3.0"))
-load(pathJoin("bacio", os.getenv("bacio_ver") or "v2.0.2"))
-load(pathJoin("ip", os.getenv("ip_ver") or "v2.0.0"))
-load(pathJoin("sp", os.getenv("sp_ver") or "v2.0.2"))
+load(pathJoin("jasper", os.getenv("jasper_ver") or "2.0.32"))
+load(pathJoin("libpng", os.getenv("libpng_ver") or "1.6.37"))
 
-load(pathJoin("ncl", os.getenv("ncl_ver") or "6.6.2"))
-load(pathJoin("nco", os.getenv("nco_ver") or "5.1.6"))
+load(pathJoin("wgrib2", os.getenv("wgrib2_ver") or "2.0.8"))
+load(pathJoin("g2", os.getenv("g2_ver") or "3.4.5"))
+load(pathJoin("g2tmpl", os.getenv("g2tmpl_ver") or "1.10.2"))
+load(pathJoin("w3emc", os.getenv("w3emc_ver") or "2.10.0"))
+load(pathJoin("w3nco", os.getenv("w3nco_ver") or "2.4.1"))
+load(pathJoin("bufr", os.getenv("bufr_ver") or "12.0.1"))
+load(pathJoin("bacio", os.getenv("bacio_ver") or "2.4.1"))
+load(pathJoin("ip", os.getenv("ip_ver") or "4.3.0"))
+load(pathJoin("sp", os.getenv("sp_ver") or "2.5.0"))
+
+-- Is this needed? It loads a newer version of netcdf, which might be messing up the version loaded above
+-- load(pathJoin("nco", os.getenv("nco_ver") or "5.0.6"))
 
 append_path("MODULEPATH", "/contrib/miniconda3/modulefiles")
 load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.12.0"))
