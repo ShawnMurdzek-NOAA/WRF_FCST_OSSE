002abad3257a406165b199e3c71b23d5ef940d01
Shawn Murdzek <shawn.s.murdzek@noaa.gov>
2025-10-27 16:56:52 -0500

    Fix WPS executables on Hercules

--- setup_exp.sh
+++ setup_exp.sh
@@ -20,9 +20,8 @@ if [[ ${machine} == 'hercules' ]]; then
 	    '/work2/noaa/wrfruc/murdzek/src/HRRR/hercules/attempt2/GSI-utils/bin/bin/read_diag_conv.x')
   pygraf_dir='/work/noaa/wrfruc/murdzek/HRRR_OSSE/exec/PYGRAF/pygraf'
   upp_exec='/work2/noaa/wrfruc/murdzek/src/HRRR/hercules/attempt2/UPP/exec/upp.x'
-  wps_exec=('/work2/noaa/wrfruc/murdzek/src/HRRR/hercules/attempt2/hrrr_wps.fd/WPSV3.9.1/geogrid.exe'
-	    '/work2/noaa/wrfruc/murdzek/src/HRRR/hercules/attempt2/hrrr_wps.fd/WPSV3.9.1/metgrid.exe'
-	    '/work2/noaa/wrfruc/murdzek/src/HRRR/hercules/attempt2/hrrr_wps.fd/WPSV3.9.1/ungrib.exe'
+  wps_exec=('/work2/noaa/wrfruc/murdzek/src/HRRR/hercules/attempt2/hrrr_wps2.fd/WPSV3.9.1/geogrid.exe'
+	    '/work2/noaa/wrfruc/murdzek/src/HRRR/hercules/attempt2/hrrr_wps2.fd/WPSV3.9.1/metgrid.exe'
 	    '/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.6.0/envs/unified-env/install/intel/2021.9.0/wgrib2-2.0.8-53fnkln/bin/wgrib2')
   wrf_exec=('/work2/noaa/wrfruc/murdzek/src/HRRR/hercules/attempt2/hrrr_update_bc.fd/hrrr_update_bc.exe'
 	    '/work2/noaa/wrfruc/murdzek/src/HRRR/hercules/attempt2/WRFV3.9_no_pnetcdf/main/real.exe'
