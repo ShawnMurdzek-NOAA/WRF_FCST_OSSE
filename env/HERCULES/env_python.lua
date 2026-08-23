bd6295f3d624817a5fd7b038d147ac31504df139
Shawn Murdzek <shawn.s.murdzek@noaa.gov>
2026-08-23 12:22:04 -0500

    Update Python environment to use miniforge

--- env/HERCULES/env_python.lua
+++ env/HERCULES/env_python.lua
@@ -4,7 +4,7 @@ This module loads libraries for running python scripts on Hercules
 
 whatis([===[Loads libraries needed for running python scripts on Hercules ]===])
 
-prepend_path("MODULEPATH", "/work2/noaa/wrfruc/murdzek/conda/miniconda_hercules/modulefiles")
-load(pathJoin("miniconda3", os.getenv("miniconda_ver") or "24.1.2"))
-setenv("PYGRAF_ENV", "/work2/noaa/wrfruc/murdzek/conda/miniconda_hercules/env/pygraf")
-setenv("MYPY_ENV", "/work2/noaa/wrfruc/murdzek/conda/miniconda_hercules/env/my_py")
+prepend_path("MODULEPATH", "/work2/noaa/wrfruc/murdzek/conda/miniforge_hercules/modulefiles")
+load(pathJoin("miniforge3", os.getenv("miniforge_ver") or "25.3.0"))
+setenv("PYGRAF_ENV", "/work2/noaa/wrfruc/murdzek/conda/miniforge_hercules/env/pygraf")
+setenv("MYPY_ENV", "/work2/noaa/wrfruc/murdzek/conda/miniforge_hercules/env/my_py")
