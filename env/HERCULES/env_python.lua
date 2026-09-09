e3b11221fae04a43ca453d71b2ef65bc67b9aa6e
Shawn Murdzek <shawn.s.murdzek@noaa.gov>
2026-09-09 16:22:52 -0500

    Update pygraf environment

--- env/HERCULES/env_python.lua
+++ env/HERCULES/env_python.lua
@@ -6,5 +6,5 @@ whatis([===[Loads libraries needed for running python scripts on Hercules ]===])
 
 prepend_path("MODULEPATH", "/work2/noaa/wrfruc/murdzek/conda/miniforge_hercules/modulefiles")
 load(pathJoin("miniforge3", os.getenv("miniforge_ver") or "25.3.0"))
-setenv("PYGRAF_ENV", "/work2/noaa/wrfruc/murdzek/conda/miniforge_hercules/env/my_pygraf")
+setenv("PYGRAF_ENV", "/work2/noaa/wrfruc/murdzek/conda/miniforge_hercules/env/pygraf")
 setenv("MYPY_ENV", "/work2/noaa/wrfruc/murdzek/conda/miniforge_hercules/env/my_py")
